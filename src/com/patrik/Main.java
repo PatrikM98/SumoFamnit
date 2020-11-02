7f0b598346385964c0f8b70a07f4142e3d01ed8a
PatrikM98 <matosevicpatrik@gmail.com>
2020-11-02 22:56:42 +0100

    Initial commit

---
+++ src/com/patrik/Main.java
@@ -0,0 +1,37 @@
+package com.patrik;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        var homeDir = System.getProperty("user.home");
+        Process p = null;
+        ProcessBuilder pb = new ProcessBuilder("python randomTrips.py -n network.net.xml -r network.rou.xml -e 50 -l");
+        pb.directory(new File(homeDir+"\\IdeaProjects\\Sumo\\data"));
+        var process  = pb.start();
+        try (var reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        }
+	/*Process p = Runtime.getRuntime().exec("python ../../../data/randomTrips.py -n network.net.xml -r network.rou.xml -e 50 -l");
+        printResults(p);
+    }
+
+    public static void printResults(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }*/
+}
