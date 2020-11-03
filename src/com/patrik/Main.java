a43c158d709a9e3ad9addac0a6a4647f0026d5ab
deljanin <pecadeljanin@gmail.com>
2020-11-03 23:08:55 +0100

    Does it work on linux???

--- src/com/patrik/Main.java
+++ src/com/patrik/Main.java
@@ -10,21 +10,50 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         String path = System.getProperty("user.dir");
+        String separator = System.getProperty("file.separator");
+        String python = getPythonEnviorment();
         ProcessBuilder builder = new ProcessBuilder(
-                "cmd.exe", "/c", "cd \"" + path + "\\data\" && python randomTrips.py -n network.net.xml -r network.rou.xml -e 50 -l");
+                "cmd.exe", "/c", "cd \"" + path + separator + "data\" && " + python + " randomTrips.py -n network.net.xml -r network.rou.xml -e 50 -l");
         builder.redirectErrorStream(true);
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line;
         while (true) {
             line = r.readLine();
-            if (line == null) { break; }
+            if (line == null) {
+                break;
+            }
             System.out.println(line);
         }
+    }
 
+    public static String getPythonEnviorment() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
 
-        System.out.println("Working Directory = " + path);
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            processBuilder = new ProcessBuilder().command("where", "python");
+        } else {
+            processBuilder.command("which", "python");
+        }
+        String path = "";
+        try {
+            Process process = processBuilder.start();
 
-
+            //read the output
+            InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String output = null;
+            while ((output = bufferedReader.readLine()) != null) {
+                path += output;
+            }
+            //wait for the process to complete
+            process.waitFor();
+            //close the resources
+            bufferedReader.close();
+            process.destroy();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return path;
     }
 }
