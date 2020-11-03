cb43382b335b7d2f1816f32208354589b7b3ddb8
PatrikM98 <matosevicpatrik@gmail.com>
2020-11-03 16:49:12 +0100

    this works

--- src/com/patrik/Main.java
+++ src/com/patrik/Main.java
@@ -1,10 +1,9 @@
 package com.patrik;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
+
 
 public class Main {
 
