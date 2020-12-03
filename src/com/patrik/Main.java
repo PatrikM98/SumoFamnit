ba61f38a9368f549c142ae9843284c61008a4167
PatrikM98 <matosevicpatrik@gmail.com>
2020-12-03 19:32:56 +0100

    Parser for routes

--- src/com/patrik/Main.java
+++ src/com/patrik/Main.java
@@ -9,9 +9,13 @@ import de.tudresden.ws.container.SumoStage;
 import de.tudresden.ws.container.SumoStringList;
 import de.tudresden.ws.container.SumoVehicleData;
 import it.polito.appeal.traci.SumoTraciConnection;
+import org.w3c.dom.Document;
 
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -21,7 +25,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //MakeRoutes();
 
-        String sumo_bin = "sumo-gui";
+        String sumo_bin = "sumo";
         String config_file = "data/config.sumocfg";
         double step_length = 0.1;
 
@@ -38,16 +42,22 @@ public class Main {
             conn.addOption("start", "true"); //start sumo immediately
             //start Traci Server
             conn.runServer();
-            for (int i = 0; i < 360000; i++) {
+
+
+            String path = System.getProperty("user.dir");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            File trips_file = new File(path + "\\data\\trips.trips.xml");
+            Document doc = builder.parse(trips_file);
+            //doc.getDocumentElement().normalize();
+            System.out.println(doc.getDocumentElement().getAttribute("trips"));
+
+            for (int i = 0; i < 3600; i++) {
 
 
                 //System.out.println();
 
-                SumoStage x = (SumoStage)conn.do_job_get(Simulation.findRoute("-150996422#0","194452010#2","DEFAULT_VEHTYPE",0,0));
-                for (String j: x.edges
-                     ) {
-                    System.out.println(j);
-                }
+                SumoStage x = (SumoStage)conn.do_job_get(Simulation.findRoute("294817175","-774963561","DEFAULT_VEHTYPE",0,0));
                 conn.do_timestep();
                 //conn.do_job_set(Vehicle.setColor(""+i, new SumoColor(255,255,51,100)));
                 conn.do_job_set(Route.add("r"+i,x.edges));
